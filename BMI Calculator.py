f470a2a77dbfada7a7b0dc817473e58634d0f4e1
CHamod-MAdusanka <madusankac965@gmail.com>
2026-06-13 13:32:23 +0530

    Delete BMI Calculator.py

--- BMI Calculator.py
+++
@@ -1,7 +0,0 @@
-import FreeSimpleGUI as FSGUI
-def calculate_bmi():
-    weight_str = FSGUI.popup_get_text(
-        "Enter your weight in Kilograms (kg):\n(ඔබේ බර කිලෝග්‍රෑම් වලින් ඇතුළත් කරන්න)",
-        title="Step 1: Weight",
-        button_color="white on blue", background_color="#2c3e50", text_color="white"
-    )
